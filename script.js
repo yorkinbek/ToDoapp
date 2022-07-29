10c4cd10f732228ec404d16ce0bd50aff202665d
yorkinbek <100893251+yorkinbek@users.noreply.github.com>
2022-07-29 16:16:32 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function add() {
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    document.querySelector('#newtask input').value = null
+}
+}
+
+function clear() {
+    document.getElementById("tasks").innerHTML = "";
+    console.log("clear working")
+    
+}
+
+document.getElementById("clear").addEventListener("click", clear)
+
+       
